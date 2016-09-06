ec9723da2516273f7ebe34a1e780ec3d21efdaa1
Cuel <Cuel@users.noreply.github.com>
2016-09-06 15:58:57 +0200

    Update index.ts

--- server/routes/index.ts
+++ server/routes/index.ts
@@ -2,5 +2,5 @@ import * as express from 'express';
 export const router: express.Router = express.Router();
 
 router.get('/', (req: express.Request, res: express.Response) => {
-    res.send('index.html');
+    res.sendFile('index.html');
 });
