c60c1be5a332e6d9879be22aeaa3be0b9d7515ae
Cuel <alleee@gmail.com>
2018-08-12 23:32:09 +0200

    inital commit

--- client/src/styles.ts
+++
@@ -1,4 +0,0 @@
-import 'purecss/build/base-min.css'
-import 'purecss/build/grids-min.css'
-import 'weather-icons/css/weather-icons.css'
-import './global.css'
--- shared/events.ts
+++
@@ -1,6 +0,0 @@
-const keymirror = require('../server/node_modules/keymirror');
-
-export const EVENTS = keymirror({
-    'reload': null,
-    'weather': null
-});
