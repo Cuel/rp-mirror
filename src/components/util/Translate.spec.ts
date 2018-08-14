3ac848222a4fdfb831ff33d8974b352c425121e0
Cuel <Cuel@users.noreply.github.com>
2018-08-14 22:16:23 +0200

    translation

--- src/components/util/Translate.spec.ts
+++ src/components/util/Translate.spec.ts
@@ -1,13 +1,27 @@
-import { translateProvider } from './Translate';
+import { translateProvider, ITranslations } from './Translate';
 
+const mock: ITranslations = {
+    default: {
+        "test": "correct eng",
+        "interpolate": "hello {test} {test}"
+    },
+    "sv-SE": {
+        "test": "correct swe",
+        "interpolate": "hello {arg1}, {arg2}"
+    }
+}
 
 describe('translateProvider', () => {
     it('returns a function bound with translations', () => {
-        const translations = {test: 'correct'}
-        const t = translateProvider(translations)
+        const t = translateProvider(mock, 'default')
         expect(t).toEqual(expect.any(Function))
-        expect(t('test')).toEqual('correct')
+        expect(t('test')).toEqual('correct eng')
+        expect(t('interpolate', { test: 'replace' })).toEqual('hello replace replace')
     })
 
-
+    it('returns a function bound with translations', () => {
+        const t = translateProvider(mock, 'sv-SE')
+        expect(t).toEqual(expect.any(Function))
+        expect(t('test')).toEqual('correct swe')
+    })
 })
--- src/components/util/Translate.ts
+++ src/components/util/Translate.ts
@@ -1,14 +1,24 @@
+interface IKeyValue { [key: string]: string }
 
-export const translateProvider = <T>(translations: T, language = 'default') => {
+export interface ITranslations {
+    default: IKeyValue
+    "sv-SE": IKeyValue
+}
+
+export const translateProvider = <T>(translations: T, language: keyof ITranslations = 'default') => {
 
     const lang = translations[language]
 
-    return function (key: keyof T) {
-        const value = lang.hasOwnProperty(key) ? lang[key] : ''
-        return translate(value)
+    return <K1 extends keyof T, K2 extends string & keyof T[K1]>(key: K2, interpolate?: IKeyValue): string => {
+        return translate(lang[key] || '', interpolate)
     }
 }
 
-const translate = () => {
+const translate = (value: string, interpolate?: IKeyValue): string => {
+    if (!interpolate) return value
 
+    Object.keys(interpolate).forEach(key => {
+        value = value.replace(new RegExp(`key`, 'g'), interpolate[key])
+    })
+    return value
 }
