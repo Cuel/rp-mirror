1be423f55e73619761df675eae77c3dd39fa8d2f
Cuel <alleee@gmail.com>
2018-08-16 01:00:02 +0200

    use cors on client

--- src/components/idg-feed/feedFetch.ts
+++ src/components/idg-feed/feedFetch.ts
@@ -1,6 +1,32 @@
 import axios from 'axios'
+const RSSParser = require('rss-parser');
 
-export const get = (async () => {
-    const feed = await axios.get('https://www.idg.se/rss/nyheter')
-    return feed
+const URL = 'https://www.idg.se/rss/nyheter'
+const CORS_PROXY = 'https://cors-anywhere.herokuapp.com/'
+
+let parser = new RSSParser();
+
+interface FeedItem {
+    title: string,
+    link: string,
+    pubDate: string,
+    content: string,
+    isoDate: string,
+    contentSnippet: string,
+    categories: string[]
+}
+
+interface Feed {
+    items: FeedItem []
+}
+
+export const get = (() => {
+    return new Promise(async (resolve, reject) => {
+        const feed = await axios.get(CORS_PROXY + URL)
+        parser.parseString(feed.data, (err: Error, newFeed: Feed) => {
+            console.log(newFeed)
+            return resolve(newFeed)
+        })
+    })
+
 })
